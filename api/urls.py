df515dd6720976c1990fb2b97d75430fdea0ed9b
Kiran Pokhrel <kiranpokhrel912@gmail.com>
2026-05-06 20:41:18 +0545

    Class-based Views

--- api/urls.py
+++ api/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('singleobj/<int:id>/', views.singleobj),
-    path('multipleobj/', views.multipleobj)
+    path('multipleobj/', views.multipleobj),
+    path('person/<int:id>/', views.SingleObjApiView.as_view()),
+    path('persons/', views.MultipleObjApiView.as_view())
 ]
--- api/views.py
+++ api/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.views import APIView
 
 from .models import Person
 from .serializers import PersonSerializer
@@ -47,3 +48,41 @@ def multipleobj(request):
         serializer.data,
         status=status.HTTP_201_CREATED
     )
+
+class MultipleObjApiView(APIView):
+    def get(self,request):
+        persons = Person.objects.all()
+        serializer = PersonSerializer(persons, many=True)
+        return Response(serializer.data)
+    
+    def post(self,request):
+        serializer = PersonSerializer(data=request.data, many=True)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+class SingleObjApiView(APIView):
+
+    def get(self, request, id):
+        person = get_object_or_404(Person, id=id)
+        serializer = PersonSerializer(person)
+        return Response(serializer.data)
+
+    def put(self, request, id):
+        person = get_object_or_404(Person, id=id)
+        serializer = PersonSerializer(person, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+
+    def patch(self, request, id):
+        person = get_object_or_404(Person, id=id)
+        serializer = PersonSerializer(person, data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+
+    def delete(self, request, id):
+        person = get_object_or_404(Person, id=id)
+        person.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
